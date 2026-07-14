8fe5ea034259a00e4ed1644572969f3d0cd9d6da
Sunil Jadhav <45939197+gitsgulabjamun@users.noreply.github.com>
2026-07-14 13:31:14 +0530

    Add PWA files

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'health-stack-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS.map(a => {
+    return new Request(a, {cache: 'reload'});
+  })).catch(() => caches.open(CACHE).then(c => c.addAll(['./index.html'])))));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  // For same-origin requests, try cache first then network
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    })));
+  }
+  // For external (sheet API), network only
+});
